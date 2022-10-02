b3397c40cf618dfd5ed569a77e5e4adf6a85d414
Leonid Zagumennov <leonzag997@gmail.com>
2022-10-02 21:46:41 +0300

    Удаление лишних файлов (должны генериться в install.py)

--- barcoder/gui/rc_resources.py
+++
@@ -1,3 +0,0 @@
-Error: RCC: Error in 'ui/resources.qrc': Cannot find file 'assets/img/barcode.png'
-
-while executing '/home/zz/development/barcoder-project/.venv/lib/python3.10/site-packages/PySide6/Qt/libexec/rcc -g python ui/resources.qrc'
